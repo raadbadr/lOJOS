9f482275a788b19db94fd2d3bc2069d1bd291d08
RaadBadr <raadbadr@gmail.com>
2020-04-27 04:11:59 +0300

    RingView Done

--- lOJOS/RingView.swift
+++ lOJOS/RingView.swift
@@ -9,27 +9,38 @@
 import SwiftUI
 
 struct RingView: View {
+    
+    var color1 = #colorLiteral(red: 1, green: 0, blue: 0, alpha: 1)
+    var color2 = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+    var width: CGFloat = 88
+    var height: CGFloat = 88
+    var percent:CGFloat = 69
+    
     var body: some View {
-        ZStack {
+       
+        let multiplier = width / 44
+        let progress = 1 - (percent / 100)
+        
+        return ZStack {
             Circle()
-                .stroke(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.2), style: StrokeStyle(lineWidth: 5))
-                .frame(width: 69, height: 69)
+                .stroke(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.2), style: StrokeStyle(lineWidth: 5 * multiplier))
+                .frame(width: width, height: height)
             Circle()
-                .trim(from: 0.1, to: 1)
+                .trim(from: progress, to: 1)
                 .stroke(
-                    LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)), Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))]), startPoint: .topTrailing, endPoint: .bottomLeading),
-                    style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0], dashPhase: 0)
+                    LinearGradient(gradient: Gradient(colors: [Color(color1), Color(color2)]), startPoint: .topTrailing, endPoint: .bottomLeading),
+                    style: StrokeStyle(lineWidth: 5 * multiplier, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0], dashPhase: 0)
             )
-            .rotationEffect(Angle(degrees: 90))
+                .rotationEffect(Angle(degrees: 90))
                 .rotation3DEffect(Angle(degrees: 180), axis: (x: 1, y:0, z:0))
-                .frame(width: 69, height: 69)
-                .shadow(color: Color(#colorLiteral(red: 0.3647058824, green: 0.06666666667, blue: 0.968627451, alpha: 1)).opacity(0.2), radius: 3, x: 0, y: 3)
+                .frame(width: width, height: height)
+                .shadow(color: Color(color2).opacity(0.2), radius: 3 * multiplier, x: 0, y: 3 * multiplier)
             
-            Text("82%")
-                .font(.subheadline)
+            Text("\(Int(percent))%")
+                .font(.system(size: 14 * multiplier))
                 .fontWeight(.bold)
         }
-            
+        
     }
 }
 
